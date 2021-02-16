fce162625b595e08c0be65a85afa53e57e4f6996
Mayank18-dotcom <mayank123chittora@gmail.com>
2021-02-16 09:57:01 +0530

    fourth commit

--- src/app/components/adduser/adduser.component.ts
+++ src/app/components/adduser/adduser.component.ts
@@ -14,10 +14,30 @@ export class User {
 
 export class AdduserComponent implements OnInit {
   newUser = new User();
+  arr : any;
+  users = [];
+  usernames = [];
+  loader = true;
   constructor(private service:AppService,public router:Router) { }
   
   ngOnInit(): void {
+    this.service.getusers().subscribe(data=>{
+      this.arr = data;
+      this.test();
+      console.log(this.usernames);
+      this.loader = false;
+    })
   }
+
+  test(){
+    this.arr.forEach(element => {
+      if(element.name){
+        this.users.push(element);
+        this.usernames.push(element.name);
+      }
+    });
+  }
+
   postone(){
     if(!this.newUser.name){
       alert("Please enter name");
@@ -29,9 +49,14 @@ export class AdduserComponent implements OnInit {
       alert("Please enter balance");
     }
     else{
-      this.service.postuser(this.newUser).subscribe(data=>{
-        this.router.navigate(['/allusers']);
-      })
+      if(this.usernames.includes(this.newUser.name)===false){
+        this.service.postuser(this.newUser).subscribe(data=>{
+          this.router.navigate(['/allusers']);
+        })
+      }
+      else{
+        alert("User already exists");
+      }
     }
   }
 
